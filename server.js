f70ab4c5395bf86dd9c3f6a33c310a943011705c
omipaul28 <omipaul28@gmail.com>
2025-09-29 00:52:41 +0600

    cors 1

--- server.js
+++ server.js
@@ -10,14 +10,16 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(cors());
-
-app.use(
-  cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://localhost:5173',
+    'https://localhost:5174',
+  ],
+  credentials: true,
+}
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(sessionMiddle);
 
